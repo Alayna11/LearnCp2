41c6b0cdf50903400bf4a4040348eeba9d894b10
Schuster_Alayna <Schuster_Alayna@G-02-12>
2022-09-15 09:05:09 -0400

    Add project files.

---
+++ LearnCp2/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace ConsoleCreatures
+    {
+        class Program
+    {
+        static void Main()
+        {
+            // This code is going to make the creature I create move around the screen using c#.
+            Console.WriteLine(" .-.");
+            Console.WriteLine("(o  o)");
+            Console.WriteLine("| O | ");
+            Console.WriteLine("|   |");
+            Console.WriteLine("'~~~'");
+            Console.WriteLine("George");
+            
+
+
+
+        }
+    }
+}
+
+
+using System;
+
+namespace Form
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create Variables
+            string name = "Shadow";
+            string breed = "Golden Retriever";
+            int age = 5;
+            double weight = 65.22;
+            bool spayed = true;
+
+            // Print variables to the console
+            Console.WriteLine(name);
+            Console.WriteLine(breed);
+            Console.WriteLine(age);
+            Console.WriteLine(weight);
+            Console.WriteLine(spayed);
+
+
+        }
+    }
+}
+
